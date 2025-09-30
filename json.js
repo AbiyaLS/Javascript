23c0a050e1ed34f4fa88915357341eaea3d306c5
AbiyaLS <abiyals9876@gmail.com>
2025-09-30 11:51:36 +0530

    Json and Dom added

---
+++ json.js
@@ -0,0 +1,27 @@
+// async function fetchData(){
+//     const response= await fetch("https://official-joke-api.appspot.com/random_joke")
+//     const data = await response.json()
+//       console.log(`${data.setup}   ${data.punchline}`)
+// }
+// fetchData()
+
+// async function postData(){
+//     try{
+//           const response = await fetch("https://jsonplaceholder.typicode.com/todos",{
+//             method : "POST",
+//             headers :{"Content-Type": "application/json"},
+//             body: JSON.stringify({
+//                 title: "Learn Fetch", 
+//                 completed: false
+//             })
+//           })
+//           const data = await response.json()
+//           console.log("Create : ",data)
+//     }
+//     catch(error){
+//            console.error(error)
+//     }
+// }
+// postData() 
+let number =[1,2,3]
+
